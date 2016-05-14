468fc0ebf9299654ffe3aa08e835ace42a3db1ef
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2016-05-14 04:03:23 +0200

    Correction d'un bug de régression sur les exports Excel

--- include/lib/export/excel.php
+++ include/lib/export/excel.php
@@ -2,7 +2,7 @@
 //
 // Export Excel
 //
-
+$couleur='#ddd';
 $tmp='';
 $tmp.='
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
